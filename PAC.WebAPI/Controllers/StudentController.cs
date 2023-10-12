b52efc9b5c19084914b9b83cb887b55ba4194074
jrabunal <juanmarabunal@gmail.com>
2023-10-12 19:25:12 -0300

    post student implementation

--- PAC.WebAPI/Controllers/StudentController.cs
+++ PAC.WebAPI/Controllers/StudentController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using PAC.BusinessLogic;
 using PAC.Domain;
 using PAC.IBusinessLogic;
 using PAC.WebAPI.Filters;
@@ -14,5 +15,18 @@ namespace PAC.WebAPI
     [Route("[controller]")]
     public class StudentController : ControllerBase
     {
+        private readonly IStudentLogic _studentLogic;
+
+        public StudentController(IStudentLogic studentLogic)
+        {
+            this._studentLogic = studentLogic;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Student value)
+        {
+            _studentLogic.InsertStudents(value);
+            return Ok();
+        }
     }
 }
