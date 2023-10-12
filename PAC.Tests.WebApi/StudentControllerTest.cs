eba6b8f9f6e6f5cdfef36ce64c4911c2d674c8c2
jrabunal <juanmarabunal@gmail.com>
2023-10-12 19:17:55 -0300

    post student test fail

--- PAC.Tests.WebApi/StudentControllerTest.cs
+++ PAC.Tests.WebApi/StudentControllerTest.cs
@@ -11,8 +11,24 @@ using Microsoft.AspNetCore.Mvc;
 [TestClass]
 public class StudentControllerTest
 {
-        [TestInitialize]
-        public void InitTest()
-        {
-        }
+    private Mock<IStudentLogic>? _studentMock;
+    private StudentController? _controller;
+
+    [TestInitialize]
+    public void InitTest()
+    {
+        _studentMock = new Mock<IStudentLogic>(MockBehavior.Strict);
+        _controller = new StudentController(_studentMock.Object);
+    }
+
+    [TestMethod]
+    public void PostStudentOk() 
+    {
+        var result = _controller.Post(_studentMock) as ObjectResult;
+        var statusCode = result.StatusCode;
+
+        _studentMock!.VerifyAll();
+        Assert.AreEqual(200, result.StatusCode);
+
+    }
 }
